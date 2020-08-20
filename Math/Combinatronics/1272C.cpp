07e15bff78b3f5b545cb26c5f68811c207d25d61
roshanajicherian <roshanaji777@gmail.com>
2020-08-20 11:04:39 +0530

    Corrected 1272C

--- Math/Combinatronics/1272C.cpp
+++ Math/Combinatronics/1272C.cpp
@@ -14,45 +14,20 @@ void solve()
         cin >> temp;
         avilableKeys.insert(temp);
     }
-    lli totalSubstring = (n * (n + 1)) / 2;
-    lli dpArray[n];
-    lli unavialbleOccuredIndex = -1;
-
+    lli ans = 0;
     for (lli i = 0; i < n; i++)
     {
-        bool anotherKey = false;
-        if (unavialbleOccuredIndex == -1 || i > unavialbleOccuredIndex)
-        {
-            for (lli j = i; j < n; j++)
-            {
-                if (avilableKeys.find(S[j]) == avilableKeys.end())
-                {
-                    anotherKey = true;
-                    unavialbleOccuredIndex = j;
-                    break;
-                }
-                else
-                    unavialbleOccuredIndex = 0;
-            }
-            if (anotherKey)
-                dpArray[i] = n - unavialbleOccuredIndex;
-            else
-                dpArray[i] = 0;
-        }
-        else
-            dpArray[i] = dpArray[i - 1];
+        lli j = i;
+        while (j < n && avilableKeys.count(S[j]) == 1)
+            j++;
+        lli length = j - i;
+        ans += ((length) * (length + 1)) / 2;
+        i = j;
     }
-    lli removedValues = 0;
-    for (lli i = 0; i < n; i++)
-    {
-        removedValues += dpArray[i];
-    }
-    cout << totalSubstring - removedValues << '\n';
+    cout << ans << '\n';
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     solve();
     return 0;
 }
